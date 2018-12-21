e592cc373e429f64dd0c67eb8eef5d3e02a0107e
Hozefa <hozefarules@gmail.com>
2018-12-20 18:49:53 -0800

    update to card.js with personal changes

--- bin/card.js
+++ bin/card.js
@@ -14,19 +14,17 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Hozefa Jodiawalla /'),
+  handle: chalk.cyan('hozefaj'),
+  work: chalk.white('Member of Technical Staff II at PayPla'),
+  twitter: chalk.cyan('https://twitter.com/HozefaJ'),
+  github: chalk.cyan('https://github.com/hozefaj'),
+  linkedin: chalk.cyan('https://www.linkedin.com/in/hozefajodiawalla/'),
+  npx: chalk.white('npx hozefaj'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
   labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
   labelCard: chalk.white.bold('      Card:')
 }
 
@@ -37,7 +35,6 @@ const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
